6e4a078693946f64a4c9dcc40b83709e9ad383f4
nguyenchung2405 <chungdaik@gmail.com>
2021-03-17 13:22:20 +0700

    BT_Glass (ôn tập State)

--- src/Components/BT_Glass/TryGlasses.js
+++ src/Components/BT_Glass/TryGlasses.js
@@ -5,13 +5,42 @@ import ProducList from './ProducList'
 import './styles.css'
 
 export default class TryGlasses extends Component {
+    arrProduct = [
+
+        { id: 1, price: 30, name: 'GUCCI G8850U', url: './glassesImage/v1.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 2, price: 50, name: 'GUCCI G8759H', url: './glassesImage/v2.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 3, price: 30, name: 'DIOR D6700HQ', url: './glassesImage/v3.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 4, price: 30, name: 'DIOR D6005U', url: './glassesImage/v4.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 5, price: 30, name: 'PRADA P8750', url: './glassesImage/v5.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 6, price: 30, name: 'PRADA P9700', url: './glassesImage/v6.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 7, price: 30, name: 'FENDI F8750', url: './glassesImage/v7.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 8, price: 30, name: 'FENDI F8500', url: './glassesImage/v8.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+        { id: 9, price: 30, name: 'FENDI F4300', url: './glassesImage/v9.png', desc: 'Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ' },
+
+    ];
+
+    state = {
+        srcImage: './glassesImage/v1.png'
+    }
+
+    thayDoiKinh = (idKinh) => {
+
+    }
 
     render() {
         return (
             <div id="bg-img">
                 <Header />
                 <Models />
-                <ProducList />
+                <ProducList arrProduct={this.arrProduct} />
             </div>
         )
     }
