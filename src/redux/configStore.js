dd5f91a6346bc0a02324414cf2c7232952de1ef5
nguyenchung2405 <chungdaik@gmail.com>
2021-03-18 20:12:05 +0700

    Cài Redux

---
+++ src/redux/configStore.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore } from 'redux';
+
+const store = combineReducers({
+    // Nơi chứa toàn bộ State của ứng dụng
+})
+
+export const store = createStore(rootReducer);
