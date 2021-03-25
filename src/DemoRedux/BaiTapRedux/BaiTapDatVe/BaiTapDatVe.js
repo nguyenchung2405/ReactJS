8f072fadc746d716dc7969ad12599d348799189a
nguyenchung2405 <chungdaik@gmail.com>
2021-03-25 19:17:15 +0700

    Buổi 9 (Bài tập đặt ghế xem phim)

--- src/DemoRedux/BaiTapRedux/BaiTapDatVe/BaiTapDatVe.js
+++ src/DemoRedux/BaiTapRedux/BaiTapDatVe/BaiTapDatVe.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import DanhSachDatGhe from './DanhSachDatGhe'
+import DanhSachGhe from './DanhSachGhe'
+import '../../../assets/video27_dat_ve_phim/BaiTapBookingTicket.css';
 
 export default class BaiTapDatVe extends Component {
     render() {
         return (
-            <div>
-                123
-            </div>
+            <>
+                <div className='row'>
+                    <div className='col-8'>
+                        <DanhSachGhe />
+                    </div>
+                    <div className='col-4'>
+                        <DanhSachDatGhe />
+                    </div>
+                </div>
+            </>
         )
     }
 }
